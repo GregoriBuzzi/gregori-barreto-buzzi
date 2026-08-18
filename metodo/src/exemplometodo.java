46ad250a6f48e48410baf6cac9188a7dd5934442
GregoriBuzzi <gregiamassa@gmail.com>
2026-08-18 19:50:42 -0300

    o

---
+++ metodo/src/exemplometodo.java
@@ -0,0 +1,19 @@
+public class exemplometodo {
+
+        public static void main(String[] args) {
+
+
+
+            desempenharCalculo(10, 6, 2);
+
+        }
+
+        public static void desempenharCalculo(double num1, double num2, double num3) {
+
+            double resultado = (num1 + num2) * num3;
+
+            System.out.println(resultado);
+        }
+
+    }
+
